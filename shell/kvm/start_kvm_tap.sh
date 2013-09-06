bf98259402c547abcc0aa3d51ab38263642008be
accesine <accesine@gmail.com>
2013-09-06 11:30:07 +0800

    add kvm start scripts

---
+++ shell/kvm/start_kvm_tap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGE=centos-zenoss-collector
+VNCDISPLAY=:3
+
+
+
+sudo /usr/libexec/qemu-kvm -enable-kvm  \
+-smp 2,sockets=1,cores=1,threads=1 \
+-drive file="${IMAGE}".qcow2,if=virtio,index=0 -boot c  \
+-m 3120 \
+-net nic \
+-net tap,ifname=tap0 \
+-nographic -vnc "${VNCDISPLAY}"
+
+
+
