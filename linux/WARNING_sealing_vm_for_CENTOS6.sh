fb2cb5675217d26e90089ae2d710564b791cf75a
root <root@inner-gateway.homenet.com>
2014-12-14 21:21:42 -0500

    refine sealing vm

--- linux/WARNING_sealing_vm_for_CENTOS6.sh
+++ linux/WARNING_sealing_vm_for_CENTOS6.sh
@@ -3,19 +3,28 @@
 #http://serverfault.com/questions/626889/what-is-the-recommended-method-to-prepare-red-hat-centos-7-templates
 
 touch /.unconfigured
-rm -rf /etc/ssh/ssh_host_*
-rm -rf /etc/udev/rules.d/70-*
+rm -f /etc/ssh/ssh_host_*
+#rm -rf /etc/udev/rules.d/70-*
 sed '/HOSTNAME=/d'  /etc/sysconfig/network
 echo "HOSTNAME=localhost.localdomain" >> /etc/sysconfig/network
 
-sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth0
-sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth1
-sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth2
-sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth3
-
-poweroff
+#sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth0
+#sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth1
+#sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth2
+#sed '/HWADDR=/d'  /etc/sysconfig/network-scripts/ifcfg-eth3
 
 
+
+ifdown eth0
+sed -i '/^HWADDR=.*$/d' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed -i '/^HWADDR=.*$/d' /etc/sysconfig/network-scripts/ifcfg-eth1
+sed -i '/^HWADDR=.*$/d' /etc/sysconfig/network-scripts/ifcfg-eth2
+sed -i '/^HWADDR=.*$/d' /etc/sysconfig/network-scripts/ifcfg-eth3
+ifup eth0
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+
+shutdown -h now
 
 
 
