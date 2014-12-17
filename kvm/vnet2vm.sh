43b894ec646d0b203208918fad509a8478be5043
root <root@inner-gateway.homenet.com>
2014-12-16 20:18:52 -0500

    add xenserver patch shell

--- kvm/vnet2vm.sh
+++ kvm/vnet2vm.sh
@@ -3,3 +3,8 @@
 VNETNAME=$1
 VNET=${VNETNAME}; for vm in $(virsh list | grep running | awk '{print $2}'); do virsh dumpxml $vm|grep -q "$VNET" && echo $vm; done
 
+
+for vm in $(virsh list | grep running | awk '{print $2}'); do echo -n "$vm:"; virsh dumpxml $vm| grep -oP "vnet\d+" ; done 
+
+
+
---
+++ xenserver/apply_patchs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# XS62ESP1002  XS62ESP1003  XS62ESP1005  XS62ESP1008  XS62ESP1009  XS62ESP1011  XS62ESP1013  XS62ESP1014  XS62ESP1015
+
+#HOSTUUID=545cdf83-947c-4d9e-aaf9-12bf0d1dec86
+#PNAME=XS62ESP1002 
+UUUID=` cd ${PNAME};xe patch-upload file-name=${PNAME}.xsupdate ; cd ..` ; xe patch-apply uuid=${UUUID} host-uuid=${HOSTUUID}; cd ..
+ 
+
